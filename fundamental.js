f8d7a195539e0f18d512666a0cf48634dfc199e4
Alwi Adam Maulana <alwiadam1@gmail.com>
2025-11-06 09:38:59 +0700

    add:variable

---
+++ fundamental.js
@@ -0,0 +1,25 @@
+//1. programing itu problem solving
+//2. errror adalah teman kita
+//3. practice
+
+//variable -> wadah atau tempat menyimpan 
+let umur = 25; // bisa diubah
+const email = "budi@gmail.com" //tidak bisa diubah
+var nama = "budi"; // cara lama
+
+//tipe data primitive
+let angka = 100; //number
+let teks = "Hello"; // string
+let benar = true; //boolean
+let kosong = null; //tipe data Null sengaja dikosongin
+let belumdiisi; //undefined (belum dikasih nilai)
+
+
+//tipe data kompleks 
+let array = [1,2,3] //array
+let object = {name: "Budi"} //object 
+
+//secara default gunakan const
+
+
+
